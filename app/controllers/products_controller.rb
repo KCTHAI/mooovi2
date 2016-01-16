97a274e1364f9ff21736c03d3726352aa543e66c
keishi <keishi@keishi-no-MacBook-Pro.local>
2016-01-16 20:59:04 +0900

    トップページに最新順で上から20件の映画作品を一覧で表示できた

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < RankingController
+  def index
+    @products = Product.order('id ASC').limit(20)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def search
+    # 検索フォームのキーワードをあいまい検索して、productsテーブルから20件の作品情報を取得する
+    @products = []
+  end
 end
