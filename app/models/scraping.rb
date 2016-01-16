4f71b5d62be485b639037074d950eef50fe2e61d
keishi <keishi@keishi-no-MacBook-Pro.local>
2016-01-16 18:52:26 +0900

    changed

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -2,11 +2,20 @@ class Scraping
   def self.movie_urls
     links = []
     agent = Mechanize.new
-    current_page = agent.get("http://eiga.com/now/")
+
+    next_url = "/now/"
+
+    while true do
+    current_page = agent.get("http://eiga.com/now/" + next_url)
     elements = current_page.search('.m_unit h3 a')
     elements.each do |ele|
       links << ele.get_attribute('href')
     end
+
+    next_link = current_page.at('.next_page')
+    next_url = next_link.get_attribute('href')
+    break unless next_url
+  end
 
     links.each do |link|
       get_product('http://eiga.com' + link)
@@ -19,7 +28,7 @@ class Scraping
     title = page.at('.moveInfoBox h1').inner_text
     image_url = page.at('.pictBox img')[:src]
 
-    product = Product.new(title: title, image_url: image_url)
+    product = Product.where(title: title, image_url: image_url).first_or_initialize
     product.save
   end
 end
