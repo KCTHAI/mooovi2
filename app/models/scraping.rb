c188bd3b6b1f9a64ce325e13d5152622c07914e6
keishi <keishi@keishi-no-MacBook-Pro.local>
2016-01-16 19:14:24 +0900

    scraping最終

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -6,29 +6,29 @@ class Scraping
     next_url = "/now/"
 
     while true do
-    current_page = agent.get("http://eiga.com/now/" + next_url)
-    elements = current_page.search('.m_unit h3 a')
-    elements.each do |ele|
-      links << ele.get_attribute('href')
+      current_page = agent.get("http://eiga.com/" + next_url)
+      elements = current_page.search('.m_unit h3 a')
+      elements.each do |ele|
+        links << ele.get_attribute('href')
     end
 
-    next_link = current_page.at('.next_page')
-    next_url = next_link.get_attribute('href')
+      next_link = current_page.at('.next_page')
+      next_url = next_link.get_attribute('href')
     break unless next_url
   end
 
-    links.each do |link|
-      get_product('http://eiga.com' + link)
+      links.each do |link|
+        get_product('http://eiga.com' + link)
     end
   end
 
   def self.get_product(link)
-    agent = Mechanize.new
-    page = agent.get(link)
-    title = page.at('.moveInfoBox h1').inner_text
-    image_url = page.at('.pictBox img')[:src]
+      agent = Mechanize.new
+      page = agent.get(link)
+      title = page.at('.moveInfoBox h1').inner_text
+      image_url = page.at('.pictBox img')[:src]
 
-    product = Product.where(title: title, image_url: image_url).first_or_initialize
-    product.save
+      product = Product.where(title: title, image_url: image_url).first_or_initialize
+      product.save
   end
 end
